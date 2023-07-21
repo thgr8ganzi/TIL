ef141f4ed5d760e7db1de4e7dcef7a087fb96c9f
jisoo <q33asq33as@gmail.com>
2023-07-22 00:39:51 +0900

    정규표현식

--- JavaScript/자바스크립트 스킬업/1부 언어/test.js
+++
@@ -1,8 +0,0 @@
-import ordinal from 'ordinal';
-import {days, months} from 'date-names';
-import {days as dayNames} from 'date-names';
-
-export function formatDate(date, format) {
-
-}
-export default ["Winter", "Spring", "Summer", "Autumn"]
