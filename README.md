271d009b414926986b7e03a53983f7056f772cf8
leejisoo <leejisoo@atnsgroup.com>
2023-08-17 16:56:41 +0900

    람다 라이브러리

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 
 * ELOQUENT JavaScript
   * link
+
+### TypeScript
+
+* 타입스크립트 프로그래밍
+  * link
+
+### Docker
+
+* 원티드
+  * link
--- TypeScript/타입스크립트 프로그래밍/ss.ts
+++ TypeScript/타입스크립트 프로그래밍/ss.ts
@@ -1,25 +1,63 @@
-const pipe = <T>(...functions: readonly Function[]): Function =>
-    (x:T): T => {
-        return functions.reduce((value, func) => func(value), x)
-    };
-const map = <T, R>(f: (T) => R) => (a: T[]): R[] => a.map(f)
-const square = value => value * value
-const squaredMap = map(square)
-const fourSquare = pipe(
-    squaredMap,
-    squaredMap
+import * as R from 'ramda'
+
+const array = [1, 2, 3]
+R.pipe(
+    R.chain(n => [n, n]),
+    R.tap(n => console.log(n)) // [ 1, 1, 2, 2, 3, 3 ]
+)(array)
+R.pipe(
+    R.chain(R.append, R.head),
+    R.tap(n => console.log(n)) // [ 1, 2, 3, 1 ]
+)(array)
+
+const flayMap = (f) => R.pipe(
+    R.map(f),
+    R.flatten
 )
+R.pipe(
+    flayMap(n => [n, n]),
+    R.tap(n => console.log(n)) // [ 1, 1, 2, 2, 3, 3 ]
+)(array)
+
+const chainTwoFunc = (firstFn, secondFn) => (x) => firstFn(secondFn(x), x)
+R.pipe(
+    chainTwoFunc(R.append, R.head),
+    R.tap(n => console.log(n)) // [ 1, 2, 3, 1 ]
+)(array)
+
+const flip = cb => a => b => cb(b)(a)
+const reversSubtract =flip(R.subtract)
+const newArray = R.pipe(
+    R.map(reversSubtract(10)), // value - 10
+    R.tap(n => console.log(n)) // [-9, -8, -7, -6, -5, -4, -3, -2, -1]
+)(R.range(1, 9 + 1))
+
+const unnest = flayMap(R.identity)
+const array2 = [[1], [2], [3]]
+R.pipe(
+    unnest,
+    R.tap(n => console.log(n)) // [ 1, 2, 3 ]
+)(array2)
+
+const always = a => b => a
+const first = <T>(a: T) => (b: T): T => always(a)(b)
+const second = <T>(a: T) => (b: T): T => flip(always)(a)(b)
 console.log(
-    fourSquare([3, 4]) // [81, 256] => [(3*3)*(3*3), (4*4)*(4*4)]
+    first(1)(2), // 1
+    second(1)(2) // 2
 )
-const reduce = <T>(f:(sum: T, value: T) => T, initValue: T) => (a:T[]): T => a.reduce(f, initValue)
-const sum = (result, value) => result + value
-const sumArray = reduce(sum, 0)
-const pitagoras = pipe(
-    squaredMap,
-    sumArray,
-    Math.sqrt
+
+const T = value => R.pipe(
+    R.applyTo(value),
+    R.tap(value => console.log(value))
 )
-console.log(
-    pitagoras([3, 4]) // 5
+const value100 = T(100)
+const sameValue = value100(R.identity) // 100
+
+const repeat = (N, cb) => R.range(1, N + 1).map(n => cb)
+const callAndAppend = R.pipe(
+    R.ap(repeat(3, R.identity)),
+    R.tap(a => console.log(a))
 )
+const input2 = [1, 2, 3]
+const result2 = callAndAppend(input2) // [1, 2, 3, 1, 2, 3, 1, 2, 3]
