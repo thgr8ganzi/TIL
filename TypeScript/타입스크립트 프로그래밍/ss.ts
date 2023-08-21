da11bc7cc4b9d6da3bf506a1e744c2f7ec5921c3
leejisoo <leejisoo@atnsgroup.com>
2023-08-21 14:58:42 +0900

    모나드

--- TypeScript/타입스크립트 프로그래밍/ss.ts
+++
@@ -1,3 +0,0 @@
-const callMap = <T, U>(fn: () => U) => <T extends {map(fn)}>(b: T) => b.map(fn)
-callMap(a => a + 1)([1])
-const cllMap2 = <T, U>(fn: () => U) => <T>(map: (fn: () => U) => T) => map(fn)
