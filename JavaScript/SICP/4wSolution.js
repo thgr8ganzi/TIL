8ba2160632fe53b814405ba44bee681464c9753a
jisoo <q33asq33as@gmail.com>
2023-10-14 18:29:49 +0900

    SCIP 4주 문제풀이

--- JavaScript/SICP/4wSolution.js
+++ JavaScript/SICP/4wSolution.js
@@ -52,4 +52,67 @@ function midpoint_segment(segment) {
 }
 const segment = make_segment(make_point(1, 2), make_point(4, 6));
 print_point(midpoint_segment(segment));
-// 2.3
+// 2.4
+function pair2(x, y) {
+    return m => m(x, y);
+}
+function head(z) {
+    return z((p, q) => p);
+}
+function tail(z) {
+    return z((p, q) => q);
+}
+console.log(head(pair2(1, 2)));
+console.log(tail(pair2(1, 2)));
+// 2.5
+// pair
+function pairCons(a, b) {
+    return Math.pow(2, a) * Math.pow(3, b);
+}
+// head
+function headCar(x) {
+    function headCarIter(x, count) {
+        if (x % 2 === 0) {
+            return headCarIter(x / 2, count + 1);
+        } else {
+            return count;
+        }
+    }
+    return headCarIter(x, 0);
+}
+// tail
+function tailCdr(x) {
+    function tailCdrIter(x, count) {
+        if (x % 3 === 0) {
+            return tailCdrIter(x / 3, count + 1);
+        } else {
+            return count;
+        }
+    }
+    return tailCdrIter(x, 0);
+}
+console.log(pairCons(2, 3));
+console.log(headCar(pairCons(2, 3)));
+console.log(tailCdr(pairCons(2, 3)));
+// 2.7
+function make_interval(a, b) {
+    return [a, b];
+}
+function lower_bound(x) {
+    return x[0];
+}
+function upper_bound(x) {
+    return x[1];
+}
+console.log(lower_bound(make_interval(1, 2)));
+console.log(upper_bound(make_interval(1, 2)));
+// 2.8
+function sub_interval(x, y) {
+    const p1 = lower_bound(x) - lower_bound(y);
+    const p2 = lower_bound(x) - upper_bound(y);
+    const p3 = upper_bound(x) - lower_bound(y);
+    const p4 = upper_bound(x) - upper_bound(y);
+    return make_interval(Math.min(p1, p2, p3, p4), Math.max(p1, p2, p3, p4));
+}
+console.log(sub_interval(make_interval(1, 2), make_interval(3, 4)));
+
