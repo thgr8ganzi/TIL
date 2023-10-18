8c3478f86c3ab67d52cc5e7c01ecfff187c148c1
jisoo <q33asq33as@gmail.com>
2023-10-18 21:15:54 +0900

    SCIP 5주 문제풀이

--- JavaScript/SICP/5wSolution.js
+++ JavaScript/SICP/5wSolution.js
@@ -1,18 +1,16 @@
 function pair(x, y) {
-    return m => m(x, y);
+    return [x, y];
 }
-function head(z) {
-    return z((p, q) => p);
-}
-function tail(z) {
-    return z((p, q) => q);
-}
-function list(...args) {
-    return args.length === 0 ? null : pair(args[0], list(...args.slice(1)));
+function list(...elements) {
+    if (elements.length === 1) {
+        return pair(elements[0], null);
+    }else if (elements.length === 0) {
+        return null;
+    }
+    const [x, ...y] = elements;
+    console.log(y);
+    return pair(x, list(...y));
 }
 // 2.17
-function lastPair(list) {
-    return list;
-}
-
-console.log(lastPair(list(23, 72, 149, 34)));
+let myList = list(23, 72, 149, 34);
+console.log(myList);
