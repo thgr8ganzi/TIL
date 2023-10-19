1d87574ed6838346d654d6948a08520e91875913
jisoo <q33asq33as@gmail.com>
2023-10-19 23:25:29 +0900

    SCIP 5주 문제풀이

--- JavaScript/SICP/5wSolution.js
+++ JavaScript/SICP/5wSolution.js
@@ -1,16 +1,28 @@
 function pair(x, y) {
     return [x, y];
 }
+// 2.17
 function list(...elements) {
     if (elements.length === 1) {
         return pair(elements[0], null);
-    }else if (elements.length === 0) {
-        return null;
     }
     const [x, ...y] = elements;
-    console.log(y);
     return pair(x, list(...y));
 }
-// 2.17
 let myList = list(23, 72, 149, 34);
-console.log(myList);
+// console.log(myList);
+// 2.18
+function reverse(list) {
+    function reverseIter(list, reversed) {
+        if (list === null) {
+            return reversed;
+        } else {
+            const [x, y] = list;
+            return reverseIter(y, pair(x, reversed));
+        }
+    }
+    return reverseIter(list, null);
+}
+// console.log(reverse(list(1, 4, 9, 16, 25)));
+// 2.20
+
