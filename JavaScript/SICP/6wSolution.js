97264ee0b1d209731de1bdd986352de8bb7cfc5a
jisoo <q33asq33as@gmail.com>
2023-10-25 22:55:18 +0900

    SCIP 6주 문제풀이

--- JavaScript/SICP/6wSolution.js
+++ JavaScript/SICP/6wSolution.js
@@ -2,3 +2,7 @@
 function squareTree(tree) {
     return tree.map(subtree => Array.isArray(subtree) ? squareTree(subtree) : Math.pow(subtree, 2));
 }
+// 2.35
+function count_leaves(t) {
+    return accumulate((x, y) => x + y, 0, map(subtree => Array.isArray(subtree) ? count_leaves(subtree) : 1, t));
+}
