7098ece5c4f5939bffb0ab768504640b9e3f97d6
jisoo <q33asq33as@gmail.com>
2023-11-01 22:53:53 +0900

    SCIP 7주 문제풀이

--- JavaScript/SICP/7wSolution.js
+++ JavaScript/SICP/7wSolution.js
@@ -4,3 +4,4 @@ const isZero = (x) => applyGeneric('isZero', x);
 put('isZero', 'schemeNumber', (x) => x === 0);
 put('isZero', 'rational', (x) => numer(x) === 0);
 put('isZero', 'complex', (x) => realPart(x) === 0 && imagPart(x) === 0);
+// 2.81
